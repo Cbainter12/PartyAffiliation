70866e79947b3695b8b931c193433a4a6c994d89
Carter <carterleebainter@gmail.com>
2023-09-22 22:01:34 -0400

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        String party = "";
+
+        System.out.println("Enter your party affiliation [R D I]: ");
+        party = in.nextLine();
+
+        if (party.equals("R"))
+        {
+            System.out.println("You get a Republican Elephant.");
+        }
+        else if (party.equals("D"))
+        {
+            System.out.println("You get a Democratic Donkey.");
+        }
+        else if (party.equals("I"))
+        {
+            System.out.println("You get an Independent Man.");
+        }
+        else
+        {
+            System.out.println("You chose other.");
+        }
     }
 }
